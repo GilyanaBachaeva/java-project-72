1215d610fae8500950424a2f0f36150f39d9947a
Gilyana Bachaeva <gelchik29@icloud.com>
2025-01-02 15:45:13 +0300

    Update ExampleTest.java

--- app/src/test/java/hexlet/code/ExampleTest.java
+++ app/src/test/java/hexlet/code/ExampleTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ExampleTest {
     @Test
-    public void testAddition1() throws Exception{
+    public void testAddition1() throws Exception {
         assertEquals(2, 1 + 1);
     }
 }
